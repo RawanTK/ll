1b91f9b1ba2a09bbffa3ebdd5190bed425d1f513
RawanTK <rooorooo@live.co.uk>
2016-01-19 18:18:05 +0300

    ..

---
+++ Simple-Android-PHP-MYSQL--master/MyApp/app/src/main/java/com/alhazmy13/myapp/AddStudent.java
@@ -0,0 +1,116 @@
+package com.alhazmy13.myapp;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AddStudent extends ActionBarActivity {
+
+    Button button;
+    EditText name,email,mobile;
+    JSONParser jsonParser;
+    ProgressDialog progressDialog;
+    int value;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_student);
+        button=(Button)findViewById(R.id.addStudentButton);
+        name=(EditText)findViewById(R.id.nameEditText);
+        email=(EditText)findViewById(R.id.emailEditText);
+        mobile=(EditText)findViewById(R.id.mobileEditText);
+        jsonParser=new JSONParser();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AddStudentTask().execute();
+            }
+        });
+
+    }
+
+    class AddStudentTask extends AsyncTask<String,String,String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog=new ProgressDialog(AddStudent.this);
+            progressDialog.setTitle("Wait...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            List<NameValuePair> list= new ArrayList<NameValuePair>();
+            list.add(new BasicNameValuePair("Name",name.getText().toString()));
+            list.add(new BasicNameValuePair("Mobile",mobile.getText().toString()));
+            list.add(new BasicNameValuePair("Email",email.getText().toString()));
+
+            JSONObject jsonObject=jsonParser.makeHttpRequest("http://192.168.1.4/myapp/add_student.php","POST",list);
+
+            try{
+              if(jsonObject!=null && !jsonObject.isNull("value")){
+                    value=jsonObject.getInt("value");
+              }else{
+                  value=0;
+              }
+
+            }catch (Exception e){
+                Log.d("ERROR",e.getMessage());
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(value==1){
+                Toast.makeText(getApplicationContext(),"Done...",Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(getApplicationContext(),"Error...",Toast.LENGTH_LONG).show();
+            }
+            progressDialog.dismiss();
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_student, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Simple-Android-PHP-MYSQL--master/PHP/db_config.php
@@ -0,0 +1,9 @@
+<?php
+
+
+header('Content-Type: text/html; charset=utf-8');
+define('DB_USER', "root"); 
+define('DB_PASSWORD', ""); 
+define('DB_DATABASE', "myapp"); 
+define('DB_SERVER', "127.0.0.1"); 
+?>

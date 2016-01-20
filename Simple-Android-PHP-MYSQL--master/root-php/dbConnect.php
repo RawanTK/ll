1bd7d702e231ca5b1890c23f4ef6d399a7d74cf7
RawanTK <rooorooo@live.co.uk>
2016-01-20 12:17:30 +0300

    ..

---
+++ Simple-Android-PHP-MYSQL--master/root-php/dbConnect.php
@@ -0,0 +1,9 @@
+<?php
+	//Change the values according to your database
+	header ('Content-Type:text/html; charset=utf-8');
+	define('HOST','mysql.hostinger.in');
+	define('USER','root');
+	define('PASS','');
+	define('DB','myapp');
+	
+	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');

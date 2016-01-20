ddb7c7c9072af37ef7fe0bbfe99b0fd1c274fce3
RawanTK <rooorooo@live.co.uk>
2016-01-20 13:16:16 +0300

    ,

--- Simple-Android-PHP-MYSQL--master/root-php/dbConnect.php
+++ Simple-Android-PHP-MYSQL--master/root-php/dbConnect.php
@@ -1,7 +1,7 @@
 <?php
 	//Change the values according to your database
 	header ('Content-Type:text/html; charset=utf-8');
-	define('HOST','mysql.hostinger.in');
+	define('HOST','localhost');
 	define('USER','root');
 	define('PASS','');
 	define('DB','myapp');
